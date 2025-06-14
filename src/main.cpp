7b3c2265c3bc471a5fdaf4c9ce0a9955b7354430
kukuikar <denis@perepetsky.ru>
2025-06-14 16:42:31 +0300

    0

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,62 @@
-#include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define NUM_BUTTONS 12
+const uint8_t ledPin = 2;
+
+struct ButtonInfo {
+  uint8_t pin;
+  bool state;
+  char name;
+};
+
+struct ButtonMessage {
+  ButtonInfo buttons[NUM_BUTTONS];
+};
+
+ButtonMessage msg;
+
+void onDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  if (len != sizeof(ButtonMessage)) return;
+
+  memcpy(&msg, incomingData, sizeof(ButtonMessage));
+
+  bool anyPressed = false;
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    if (msg.buttons[i].state) {
+      anyPressed = true;
+      break;
+    }
+  }
+
+  digitalWrite(ledPin, anyPressed ? HIGH : LOW);
+
+  Serial.print("Received states: ");
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    Serial.print(msg.buttons[i].name);
+    Serial.print("=");
+    Serial.print(msg.buttons[i].state ? "ON " : "OFF ");
+  }
+  Serial.println();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("ESP-NOW init failed");
+    while (true);
+  }
+
+  esp_now_register_recv_cb(onDataRecv);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Всё обрабатывается в прерывании приёма
 }
